65df4db5d34210bd89c0687525e76f6e10385bf1
0010capacity <0010capacity@gmail.com>
2025-12-06 10:52:24 +0900

    Remove problematic delete_all_data migration

--- backend/migrations/20240101000009_delete_all_data.sql
+++
@@ -1,8 +0,0 @@
--- Delete all data from tables (but keep structure)
-DELETE FROM novel_relations;
-DELETE FROM novel_chapters;
-DELETE FROM novels;
-DELETE FROM blog_posts;
-DELETE FROM app_distributions;
-DELETE FROM apps;
-DELETE FROM admins;
