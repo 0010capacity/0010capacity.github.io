d553707e2660c28d58bb953abfcf88f28d070fcc
0010capacity <0010capacity@gmail.com>
2025-12-06 10:45:15 +0900

    Delete all data from database

---
+++ backend/migrations/20240101000009_delete_all_data.sql
@@ -0,0 +1,8 @@
+-- Delete all data from tables (but keep structure)
+DELETE FROM novel_relations;
+DELETE FROM novel_chapters;
+DELETE FROM novels;
+DELETE FROM blog_posts;
+DELETE FROM app_distributions;
+DELETE FROM apps;
+DELETE FROM admins;
